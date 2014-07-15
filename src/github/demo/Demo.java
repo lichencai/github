450bcc9ab30745931943f9c5839051f19f991090
lichencai <793385991@qq.com>
2014-07-15 17:38:09 +0800

    文件读取

--- src/github/demo/Demo.java
+++ src/github/demo/Demo.java
@@ -1,7 +1,38 @@
 package github.demo;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+
 public class Demo {
-	public static void main(String[] args){
-		System.out.println("hello word githut");
+	public static void main(String[] args) throws Exception{
+		String inputPath = "E:\\file.txt";
+		String outputPath = "E:\\file1.txt";
+		
+		FileInputStream fis = new FileInputStream(inputPath);
+		InputStreamReader isr = new InputStreamReader(fis, "utf-8");
+		BufferedReader br = new BufferedReader(isr);
+		StringBuffer sb = new StringBuffer();
+		String str = "";
+		while ((str = br.readLine()) != null) {
+			 sb.append(str);
+		}
+		br.close();
+		System.out.println(sb.toString());
+		
+		/*File inputFile = new File(inputPath);
+		Scanner read = new Scanner(inputFile);
+		StringBuffer sb = new StringBuffer();
+		while(read.hasNextLine()){
+			sb.append(read.nextLine());
+		}
+		read.close();
+		
+		File outputFile = new File(outputPath);
+		PrintWriter pw = new PrintWriter(outputFile);
+		pw.write(sb.toString());
+		pw.flush();
+		pw.close();*/
 	}
 }
